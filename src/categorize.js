f07e384c76d8248d145274a84d5a22ab7fb80bba
Imtiaz Rahman <imtiazrahman@Imtiazs-MBP.fios-router.home>
2020-07-26 17:50:53 -0400

    fixed liner and removed warnings for categorize

--- src/categorize.js
+++ src/categorize.js
@@ -20,14 +20,14 @@ class Categorize extends Component{
 
   componentDidMount(){
     var user = getFirebase().auth().currentUser;
-    var name, email, photoUrl, uid, emailVerified;
+    var name, email, photoUrl;
 
     if (user != null) {
       name = user.displayName;
       email = user.email;
       photoUrl = user.photoURL;
-      emailVerified = user.emailVerified;
-      uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+      //emailVerified = user.emailVerified;
+      //uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
                        // this value to authenticate with your backend server, if
 
     }
