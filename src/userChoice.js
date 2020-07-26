5fffa6159f4a0dad27086f8bf4b55d2d529a67bd
Judy <judy.zhang67@myhunter.cuny.edu>
2020-07-26 17:19:02 -0400

    getfilterList funct
    - added filterlist.text
    - fixed liner in function

--- src/userChoice.js
+++ src/userChoice.js
@@ -8,6 +8,7 @@ const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
 const lineByLine = require("n-readlines");
+const chalk = require("chalk");
 
 // If modifying these scopes, delete token.json.
 //const SCOPES = ['https://www.googleapis.com/auth/gmail.modify'];
@@ -44,101 +45,6 @@ function authorize(credentials, callback) {
     callback(oAuth2Client);
   });
 }
-// uses the gmail.users.messages.list to list the messages that meet the criteria given
-function listMessages(auth, query) {
-  return new Promise((resolve, reject) => {
-    const gmail = google.gmail({version: 'v1', auth});
-    gmail.users.messages.list(
-      {
-        userId: 'me',
-        q: query,
-      },
-      (err, res) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        if (!res.data.messages) {
-          resolve([]);
-          return;
-        }
-        resolve(res.data.messages);
-      }
-    );
-  });
-}
-
-
-//getfilter list
-
-// reads filters and gets the eamils according to the filters
-const getFilterList = (fileName) => {
-  const filteringList = [];
-  const liner = new lineByLine(fileName);
-  while ((line = liner.next())) {
-    filteringList.push(line.toString("utf8"));
-  }
-  return filteringList;
-};
-
-
-// creating Labels
-function modifyLabels(auth, messageId, addLabelIds, removeLabelIds) {
-  return new Promise((resolve, reject) => {
-    const gmail = google.gmail({version: 'v1', auth});
-    gmail.users.messages.modify(
-      {
-        id: messageId,
-        userId: 'me',
-        resource: {
-          addLabelIds,
-          removeLabelIds,
-        },
-      },
-      (err, res) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(res);
-        return;
-      }
-    );
-  });
-  messages.forEach(msg => {
-    modifyLabels(oAuth2Client, msg.id, [Label_11], ['INBOX']);
-})
-}
-
-//create filter
-function createFilter(auth){
-  var gmail = google.gmail('v1');
-  var data = {
-    "criteria": {
-      "from": "someone@gmail.com",
-    },
-    "action": {
-    "addLabelIds": [
-      "CLUNK"
-    ],
-    "removeLabelIds": [
-      "INBOX"
-    ],
-    },
-  };
-  gmail.users.settings.filters.create({
-    auth: auth,
-    userId: 'me',
-    resource: data,
-  }, function(err, result) {
-    if (err) {
-      console.log( err);
-    } else {
-      console.log( result );
-      callback( result );
-    }
-  });
-}
 
 /**
  * Get and store new token after prompting for user authorization, and then
@@ -170,6 +76,175 @@ function getNewToken(oAuth2Client, callback) {
     });
   });
 }
+
+// uses the gmail.users.messages.list to list the messages that meet the criteria given
+// function listMessages(auth, query) {
+//   return new Promise((resolve, reject) => {
+//     const gmail = google.gmail({version: 'v1', auth});
+//     gmail.users.messages.list(
+//       {
+//         userId: 'me',
+//         q: query,
+//       },
+//       (err, res) => {
+//         if (err) {
+//           reject(err);
+//           return;
+//         }
+//         if (!res.data.messages) {
+//           resolve([]);
+//           return;
+//         }
+//         resolve(res.data.messages);
+//       }
+//     );
+//   });
+// }
+
+
+//getfilter list
+
+// reads filters and gets the eamils according to the filters
+const getFilterList = (fileName) => {
+  const filteringList = [];
+  const liner = new lineByLine(fileName);
+  while ((liner = liner.next())) {
+    filteringList.push(liner.toString("utf8"));
+  }
+  return filteringList;
+};
+
+async function deleteMessageWrapper(auth) {
+  console.log(
+    chalk.blue(
+      `Started delete operation-------------------------------------------------`
+    )
+  );
+  const batchLimit = 50;
+  const filteringList = getFilterList("filterlist.text");
+  let finalResult = [];
+  let result = await filteringList.forEach(async (item) => {
+    let result = await getMessagesByFilter(auth, item);
+    let noOfBatches = Math.ceil(result.length / batchLimit);
+    for (let i = 0; i < noOfBatches; i++) {
+      let ids = [];
+      for (let j = i * batchLimit; j < i * batchLimit + batchLimit; j++) {
+        if (result[j]) {
+          ids.push(result[j]);
+        } else {
+          break;
+        }
+      }
+      let resultNew = await deleteBatch(auth, ids);
+    }
+    finalResult.push({
+      Filter: item,
+      Mails_Fetched: result.length,
+    });
+    if (filteringList.length === finalResult.length) {
+      console.log(
+        chalk.red(
+          `Deleted items summary----------------------------------------------------`
+        )
+      );
+      console.table(finalResult);
+    }
+  });
+}
+
+async function deleteBatch(auth, list) {
+  const gmail = google.gmail({ version: "v1", auth });
+  let res = await gmail.users.messages.batchDelete({
+    userId: "me",
+    resource: { ids: list },
+  });
+}
+
+async function getMessagesByFilter(auth, filter, token = "", result = []) {
+  const gmail = google.gmail({ version: "v1", auth });
+  let res = await gmail.users.messages.list({
+    userId: "me",
+    maxResults: 500,
+    pageToken: token,
+    q: filter,
+  });
+
+  if (res && res.data) {
+    let messages = res.data.messages;
+    if (messages && messages.length) {
+      messages.forEach((message) => result.push(message.id));
+      if (res.data.nextPageToken) {
+        result = await getMessagesByFilter(
+          auth,
+          filter,
+          res.data.nextPageToken,
+          result
+        );
+      }
+    }
+  }
+  return result;
+}
+
+
+
+// // creating Labels
+// function modifyLabels(auth, messageId, addLabelIds, removeLabelIds) {
+//   return new Promise((resolve, reject) => {
+//     const gmail = google.gmail({version: 'v1', auth});
+//     gmail.users.messages.modify(
+//       {
+//         id: messageId,
+//         userId: 'me',
+//         resource: {
+//           addLabelIds,
+//           removeLabelIds,
+//         },
+//       },
+//       (err, res) => {
+//         if (err) {
+//           reject(err);
+//           return;
+//         }
+//         resolve(res);
+//         return;
+//       }
+//     );
+//   });
+//   messages.forEach(msg => {
+//     modifyLabels(oAuth2Client, msg.id, [Label_11], ['INBOX']);
+// })
+// }
+
+//create filter
+// function createFilter(auth){
+//   var gmail = google.gmail('v1');
+//   var data = {
+//     "criteria": {
+//       "from": "someone@gmail.com",
+//     },
+//     "action": {
+//     "addLabelIds": [
+//       "CLUNK"
+//     ],
+//     "removeLabelIds": [
+//       "INBOX"
+//     ],
+//     },
+//   };
+//   gmail.users.settings.filters.create({
+//     auth: auth,
+//     userId: 'me',
+//     resource: data,
+//   }, function(err, result) {
+//     if (err) {
+//       console.log( err);
+//     } else {
+//       console.log( result );
+//       callback( result );
+//     }
+//   });
+// }
 
 
 /**
